a908a99e3365e687fef4bdbe89b2f948b8561110
Berke Maltepe <34270476+BerkeMaltepe@users.noreply.github.com>
2023-10-30 15:43:41 -0400

    Implemented Textual View

---
+++ hw5/src/View/ReversiTextualView.java
@@ -0,0 +1,50 @@
+package View;
+
+
+import Model.HexState;
+import Model.Hexagon;
+import Model.IReversi;
+
+public class ReversiTextualView implements IView{
+
+  public void render(IReversi model){
+    String view = "";
+    Hexagon[][] board = null;
+
+    board = model.getBoard();
+
+    int afterHalf = 1;
+    for (int i = 0; i < board.length; i++) {
+
+
+      if (i > (board.length / 2)) {
+        for (int b = 0; b < afterHalf; b++){
+          view += " ";
+        }
+        afterHalf++;
+      }
+
+      for (int j = 0; j < board.length; j++) {
+
+
+
+        if (board[i][j] == null) {
+          view += " ";
+        } else if (board[i][j].isEmpty()) {
+          view += "_ ";
+        } else if (board[i][j].getDiscOnHex() == HexState.BLACK) {
+          view += "X ";
+        } else if (board[i][j].getDiscOnHex() == HexState.WHITE) {
+          view += "O ";
+        }
+      }
+      view += "\n";
+    }
+
+    System.out.print(view);
+
+
+
+  }
+
+}
