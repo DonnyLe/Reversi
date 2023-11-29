d186de1fffa93e56d3109ec4b96d70d1ad8ca78e
Berke Maltepe <34270476+BerkeMaltepe@users.noreply.github.com>
2023-11-28 23:18:34 -0500

    a

---
+++ hw7/src/controller/HumanPlayer.java
@@ -0,0 +1,16 @@
+package controller;
+
+import model.AxialCoord;
+
+public class HumanPlayer implements IPlayer{
+
+  @Override
+  public AxialCoord move() {
+    return null;
+  }
+
+  @Override
+  public void pass() {
+
+  }
+}
